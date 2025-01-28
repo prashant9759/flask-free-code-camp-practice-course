80dd1cf5862cc680103404d44cf9a98ed90df982
prashant9759 <prashantkumar.20255@gmail.com>
2025-01-28 17:35:58 +0000

    Added a html page and an api route to list jobs

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
+jobs = [{
+    "id": 1,
+    "title": "Data Analyst",
+    "location": "Bengaluru, India",
+    "salary": "Rs. 10,00,000"
+}, {
+    "id": 2,
+    "title": "Data Scientist",
+    "location": "Delhi, India",
+    "salary": "Rs. 15,00,000"
+}, {
+    "id": 3,
+    "title": "Frontend Engineer",
+    "location": "San Francisco, USA",
+    "salary": "$120,000"
+}, {
+    "id": 4,
+    "title": "Backend Engineer",
+    "location": "San Francisco, USA",
+    "salary": "$140,000"
+}, {
+    "id": 5,
+    "title": "Software Engineer",
+    "location": "San Francisco, USA",
+    "salary": "$150,000"
+}]
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(jobs)
+
+
 @app.route("/")
 def hello():
-  return jsonify({"messsage": "Hello World"})
+  return render_template("home.html")
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+  app.run(host="0.0.0.0", debug=True)
